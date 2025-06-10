4abfb81f11343bb337061880a199ca9106956525
Emmanuel Lopez <emlopezdev@gmail.com>
2025-06-10 16:51:12 -0400

    added javasctip to show/hide sidebar

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const hamburger = document.getElementById("hamburger");
+const closeButton = document.getElementById("close");
+const sidebar = document.getElementById("sidebar");
+const sidebarLinks = document.querySelectorAll(".sidebar__link");
+
+const showSidebar = () => {
+    sidebar.style.display = "block";
+};
+
+const hideSidebar = () => {
+    sidebar.style.display = "none";
+};
+
+const offsetNav = (evt) => {
+    evt.preventDefault();
+    const targetId = evt.target.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+        const headerHeight = document.getElementById("header").offsetHeight;
+        const targetPosition = targetElement.offsetTop - headerHeight;
+
+        window.scrollTo({
+            top: targetPosition,
+        });
+    }
+
+    hideSidebar();
+};
+
+hamburger.addEventListener("click", showSidebar);
+
+closeButton.addEventListener("click", hideSidebar);
+
+sidebarLinks.forEach((link) => {
+    link.addEventListener("click", offsetNav);
+});
+
+window.addEventListener("resize", () => {
+    if (this.innerWidth > 1100) {
+        hideSidebar();
+    }
+});
