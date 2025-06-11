519154a63f2e3b7b4fe2cb872392ddf9e9538f4a
Emmanuel Lopez <emlopezdev@gmail.com>
2025-06-11 12:03:42 -0400

    Updated sidebar styling
    Added animation to sidebar when hidding and showing. Added media query
    reduce motion to animation for accessiblity. Also added ability to close
    sidebar when clicking on the overlay/container.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 const hamburger = document.getElementById("hamburger");
 const closeButton = document.getElementById("close");
+const sidebarContainer = document.getElementById("sidebar-container");
 const sidebar = document.getElementById("sidebar");
 const sidebarLinks = document.querySelectorAll(".sidebar__link");
 
 const showSidebar = () => {
-    sidebar.style.display = "block";
+    sidebarContainer.style.display = "block";
+    setTimeout(() => {
+        sidebar.classList.add("is-open");
+    }, 0);
 };
 
 const hideSidebar = () => {
-    sidebar.style.display = "none";
+    sidebar.classList.remove("is-open");
+    setTimeout(() => {
+        sidebarContainer.style.display = "none";
+    }, 200);
 };
 
 const offsetNav = (evt) => {
@@ -34,6 +41,12 @@ closeButton.addEventListener("click", hideSidebar);
 
 sidebarLinks.forEach((link) => {
     link.addEventListener("click", offsetNav);
+});
+
+sidebarContainer.addEventListener("click", (e) => {
+    if (e.target.id === "sidebar-container") {
+        hideSidebar();
+    }
 });
 
 window.addEventListener("resize", () => {
